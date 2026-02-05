073f0bc6fc3eed424d3acf50050e8ccdd4d6bff4
iamanuchiwa <mercyjoshuakodjo@gmail.com>
2026-02-05 14:31:19 -0500

    Code pour la partie analytique

--- src/main.py
+++ src/main.py
@@ -3,3 +3,16 @@ import matplotlib.pyplot as plt
 import math
 y=7
 x = np.linspace(-10, 10, 400)
+
+# --- Paramètres Physiques ---
+Deff = 1e-10        # Coefficient de diffusion effectif [m^2/s]
+S = 2e-8            # Terme source constant [mol/m^3/s]
+Ce = 20.0           # Concentration à la surface [mol/m^3]
+R_pilier = 0.5      # Rayon du pilier (Diamètre = 1m)
+
+# --- Solution Analytique (Eq. 2) ---
+def solution_analytique(r):
+    #C(r) = (S / 4Deff) * R^2 * ((r^2/R^2) - 1) + Ce
+    terme1 = (S / (4 * Deff)) * (R_pilier**2)
+    terme2 = ((r**2) / (R_pilier**2)) - 1
+    return terme1 * terme2 + Ce
